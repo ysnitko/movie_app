cd4315e6ee8b84c9b94ca54d81cbddf1163dcd3b
yuri_sn <yuri.snitko@gmail.com>
2023-05-14 00:22:16 +0300

    movie

---
+++ script/fentch.js
@@ -0,0 +1,145 @@
+const searchParams = new URLSearchParams(location.search);
+const id = searchParams.get('id') || 'all';
+const characterID = searchParams.get('character');
+
+loadAllMovies(id);
+
+async function loadMovie(id) {
+  const response = await fetch(
+    `https://desfarik.github.io/star-wars/api/film/${id}.json`
+  );
+  const data = await response.json();
+  return data;
+}
+
+async function loadAllMovies(id) {
+  const movieItems = document.querySelector('.movie-items');
+  const promises = loadMovie(id);
+  let movieList = await Promise.resolve(promises);
+  movieList
+    .map((movie) => {
+      const linkMovie = document.createElement('a');
+      linkMovie.classList.add('movie-item');
+      linkMovie.setAttribute('data-id', `${movie.id}`);
+      linkMovie.setAttribute('href', `index.html?id=${movie.id}`);
+      const movieCover = MOVIE_COVER.find((cover) => cover.id === movie.id);
+      html = `
+      <div class="img-container">
+      <img src="${movieCover.src}" class="movie-cover" alt="movie"> 
+      <span class="movie-rating">${movieCover.rating}</span> 
+      </div>
+      <div class="movie-info">
+        <div class="movie-title"><span>${movie.title}</span></div>
+        <div class="movie-crawl"><span>${movie.opening_crawl}</span></div>
+        <div class="movie-created"><span>Release date: ${movie.release_date}</span></div>
+        <div class="movie-episode"><span>Episode: ${movie.episode_id}</span></div>
+      </div>`;
+      linkMovie.innerHTML = html;
+      return linkMovie;
+    })
+    .forEach((movie) => movieItems.appendChild(movie));
+  OnSortingItems();
+}
+
+async function renderMovieAbout(id) {
+  const movie = await loadMovie(id);
+  const movieContainer = document.querySelector('.movies-container');
+  const movieCover = MOVIE_COVER.find((cover) => cover.id === movie.id);
+  const html = `<div class="movie-about">
+  <div class="movie-header">           
+      <span>Star Wars: ${movie.title}. Episode ${movie.episode_id}</span>
+      <a class="add-favorites"><img src="./img/bookmark-add.svg" alt=""><span>Add to favorites</span></a>
+  </div>
+  <img class="img-about" src="${movieCover.src_about}" alt="">
+  <div class="movie-additional-info">
+      <div class="realese-reating-info">
+          <p>Release date: <span>${movie.release_date}</span></p>
+          <p>Rating IMDb: <span>${movieCover.rating}</span></p>
+      </div>
+      <div class="director-produser-info">
+          <p>Directod by: <span>${movie.director}</span></p>
+          <p>Produced by: <span>${movie.producer}</span></p>
+      </div>
+  </div>
+  <div class="movie-story">
+      <h3>Plot:</h3>
+      <p>${movie.opening_crawl}</p>
+  </div>
+  <div class="characters">
+    <span>Characters:</span>
+    <div class="characters-container"></div>
+  </div>
+`;
+  movieContainer.innerHTML = html;
+  loadMovieCharacters(id);
+}
+
+renderMovieAbout(id);
+
+async function loadCharacter(id) {
+  const response = await fetch(
+    `https://desfarik.github.io/star-wars/api/people/${id}.json`
+  );
+  const data = await response.json();
+  return data;
+}
+
+async function loadMovieCharacters(id) {
+  const charactersContainer = document.querySelector('.characters-container');
+  const promises = Array.from({ length: 87 }, (_, i) => loadCharacter(i + 1));
+  let charactersList = await Promise.all(promises);
+  let movie = await loadMovie(id);
+  charactersList
+    .filter((character) => {
+      return character.films.includes(`${movie.id}`);
+    })
+    .map((character) => {
+      const characterLink = document.createElement('a');
+      characterLink.classList.add('character-link');
+      characterLink.setAttribute(
+        'href',
+        `index.html?character=${character.id}`
+      );
+      const html = `  
+      <div>
+      <img src="${character.image}" class="movie-cover" alt="movie"> 
+      </div>
+      <div class="character-info">
+        <div class="character-title"><span>name: ${character.name}</span></div>
+        <div class="character-created"><span>species: ${character.species}</span></div>
+        <div class="character-created"><span>homeworld: ${character.homeworld}</span></div>
+      </div>`;
+      characterLink.innerHTML = html;
+      return characterLink;
+    })
+    .forEach((character) => charactersContainer.appendChild(character));
+}
+
+async function loadCharacterInfo(id) {
+  const response = await fetch(
+    `https://desfarik.github.io/star-wars/api/people/${id}.json`
+  );
+  const data = await response.json();
+  // console.log(data);
+  return data;
+}
+
+async function renderCharacterAbout(id) {
+  const characterInfo = await loadCharacterInfo(id);
+  const movieContainer = document.querySelector('.movies-container');
+  const html = `<div class="movie-about"> 
+    <div>            
+      <span>Star Wars: ${characterInfo.name}. Episode ${characterInfo.eye_color}</span> 
+      <img class="img-about" src="${characterInfo.image}" alt=""> 
+    </div> 
+    <div class="character-info">
+      <div class="character-birth"><span>Birth year: ${characterInfo.birth_year}</span></div> 
+      <div class="character-gender"><span>Gender: ${characterInfo.gender}</span></div> 
+      <div class="character-height"><span>Height: ${characterInfo.height}</span></div> 
+      <div class="character-created"><span>Homeworld: ${characterInfo.homeworld}</span></div> 
+    </div>
+  </div>`;
+  movieContainer.innerHTML = html;
+}
+
+renderCharacterAbout(characterID);
--- script/script.js
+++ script/script.js
@@ -1,120 +1,7 @@
 const select = document.querySelector('#sorting');
 const toggleLayout = document.querySelector('.action-toggle-layout');
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('id') || 'all';
 
-loadMore(id);
-
-async function loadMovie(id) {
-  const response = await fetch(
-    `https://desfarik.github.io/star-wars/api/film/${id}.json`
-  );
-  const data = await response.json();
-  console.log(data);
-  return data;
-}
-
-async function loadMore(id) {
-  const movieItems = document.querySelector('.movie-items');
-  const promises = loadMovie(id);
-  let movieList = await promises;
-  movieList
-    .map((movie) => {
-      const linkMovie = document.createElement('a');
-      linkMovie.classList.add('movie-item');
-      linkMovie.setAttribute('data-id', `${movie.id}`);
-      linkMovie.setAttribute('href', `index.html?id=${movie.id}`);
-      const movieCover = MOVIE_COVER.find((cover) => cover.id === movie.id);
-      html = `
-      <div class="img-container">
-      <img src="${movieCover.src}" class="movie-cover" alt="movie"> 
-      <span class="movie-rating">${movieCover.rating}</span> 
-      </div>
-      <div class="movie-info">
-        <div class="movie-title"><span>${movie.title}</span></div>
-        <div class="movie-crawl"><span>${movie.opening_crawl}</span></div>
-        <div class="movie-created"><span>Release date: ${movie.release_date}</span></div>
-        <div class="movie-episode"><span>Episode: ${movie.episode_id}</span></div>
-      </div>`;
-      linkMovie.innerHTML = html;
-      return linkMovie;
-    })
-    .forEach((movie) => movieItems.appendChild(movie));
-  OnSortingChange();
-}
-
-async function renderMovie(id) {
-  const movie = await loadMovie(id);
-  const movieContainer = document.querySelector('.movies-container');
-  const movieCover = MOVIE_COVER.find((cover) => cover.id === movie.id);
-  const html = `<div class="movie-about">
-  <div class="movie-header">           
-      <span>Star Wars: ${movie.title}. Episode ${movie.episode_id}</span>
-      <a class="add-favorites"><img src="./img/bookmark-add.svg" alt=""><span>Add to favorites</span></a>
-  </div>
-  <img class="img-about" src="${movieCover.src_about}" alt="">
-  <div class="movie-additional-info">
-      <div class="realese-reating-info">
-          <p>Release date: <span>${movie.release_date}</span></p>
-          <p>Rating IMDb: <span>${movieCover.rating}</span></p>
-      </div>
-      <div class="director-produser-info">
-          <p>Directod by: <span>${movie.director}</span></p>
-          <p>Produced by: <span>${movie.producer}</span></p>
-      </div>
-  </div>
-  <div class="movie-story">
-      <h3>Plot:</h3>
-      <p>${movie.opening_crawl}</p>
-  </div>
-  <div class="characters">
-    <span>Characters:</span>
-    <div class="characters-container"></div>
-  </div>
-`;
-  movieContainer.innerHTML = html;
-  loadMovieCharacters(id);
-}
-renderMovie(id);
-
-async function loadCharacter(id) {
-  const response = await fetch(
-    `https://desfarik.github.io/star-wars/api/people/${id}.json`
-  );
-  const data = await response.json();
-  console.log(data);
-  return data;
-}
-async function loadMovieCharacters(id) {
-  const charactersContainer = document.querySelector('.characters-container');
-  const promises = Array.from({ length: 87 }, (_, i) => loadCharacter(i + 1));
-  let charactersList = await Promise.all(promises);
-  console.log(charactersList);
-  let movie = await loadMovie(id);
-  charactersList
-    .filter((character) => {
-      console.log(character.films);
-      return character.films.includes(`${movie.id}`);
-    })
-    .map((character) => {
-      const characterItem = document.createElement('a');
-      characterItem.classList.add('character-link');
-      const html = `  
-      <div>
-      <img src="${character.image}" class="movie-cover" alt="movie"> 
-      </div>
-      <div class="character-info">
-        <div class="character-title"><span>name: ${character.name}</span></div>
-        <div class="character-created"><span>species: ${character.species}</span></div>
-        <div class="character-created"><span>homeworld: ${character.homeworld}</span></div>
-      </div>`;
-      characterItem.innerHTML = html;
-      return characterItem;
-    })
-    .forEach((character) => charactersContainer.appendChild(character));
-}
-
-function OnSortingChange() {
+function OnSortingItems() {
   const movieItems = document.querySelector('.movie-items');
   const movieList = Array.from(document.querySelectorAll('.movie-item'));
   if (select.selectedIndex === 0) {
@@ -219,8 +106,8 @@ toggleLayout.addEventListener('click', OnChangeLayout);
 async function searchItems(event) {
   event.preventDefault();
   const searchInput = document.querySelector('#search-input');
-  const phrase = searchInput.value.toLowerCase().trim();
   const movieItems = document.querySelector('.movie-items');
+  const phrase = searchInput.value.toLowerCase();
   movieItems.innerHTML = '';
   const movieList = await loadMovie(id);
   movieList
@@ -230,11 +117,9 @@ async function searchItems(event) {
     .forEach((movie) => {
       const linkMovie = document.createElement('a');
       linkMovie.classList.add('movie-item');
-      linkMovie.setAttribute('href', `${renderMovie(movie.episode_id)}`);
-      linkMovie.setAttribute('data-id', `${movie.episode_id}`);
-      const movieCover = MOVIE_COVER.find(
-        (cover) => cover.id === movie.episode_id
-      );
+      linkMovie.setAttribute('data-id', `${movie.id}`);
+      linkMovie.setAttribute('href', `index.html?id=${movie.id}`);
+      const movieCover = MOVIE_COVER.find((cover) => cover.id === movie.id);
       html = `
       <div class="img-container">
       <img src="${movieCover.src}" class="movie-cover" alt="movie"> 
@@ -265,11 +150,10 @@ function restoreUsers() {
 }
 
 function toggleTheme() {
-  const link = document.querySelector('link');
-  const checked = document.querySelector('#checkbox');
-  if (checked.checked) {
-    link.setAttribute('href', './style/dark-theme.css');
+  const toggleTheme = document.querySelector('#checkbox');
+  if (toggleTheme.checked) {
+    document.body.classList.add(`dark-theme`);
   } else {
-    link.setAttribute('href', './style/style.css');
+    document.body.classList.remove(`dark-theme`);
   }
 }
